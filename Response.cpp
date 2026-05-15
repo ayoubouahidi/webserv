3c865d8b59beda2cb3ff03ef39b6e2b020af62eb
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2026-05-15 20:16:20 +0100

    building respond

--- Response.cpp
+++ Response.cpp
@@ -1,7 +1,7 @@
 #include "Response.hpp"
 #include <sstream>
 
-Response::Response() : code(200), message("OK") {
+Response::Response() : code(200), _statusMessage("OK") {
     std::cout << "Response created with default constructor" << std::endl;
 }
 
@@ -9,55 +9,96 @@ Response::~Response() {
     std::cout << "Response destroyed" << std::endl;
 }
 
-Response::Response(const Response& other) : code(other.code), message(other.message), headers(other.headers), body(other.body) {
+Response::Response(const Response& other) : code(other.code), _statusMessage(other._statusMessage), headers(other.headers), body(other.body) {
     std::cout << "Response created with copy constructor" << std::endl;
 }
 
 Response& Response::operator=(const Response& other) {
     if (this != &other) {
         code = other.code;
-        message = other.message;
+        _statusMessage = other._statusMessage;
         headers = other.headers;
         body = other.body;
     }
     return *this;
 }
 
-Response::Response(int code, const std::string& message) : code(code), message(message) {
-    std::cout << "Response created with parameterized constructor" << std::endl;
-}
-
 
 int Response::getCode() const {
     return code;
 }
-const std::string& Response::getMessage() const {
-    return message;
-}
-const std::map<std::string, std::string>& Response::getHeaders() const {
-    return headers;
-}
-const std::string& Response::getBody() const {
-    return body;
-}
 
-void Response::addHeader(const std::string& key, const std::string& value) {
-    headers[key] = value;
-}
 
-void Response::setBody(const std::string& body) {
-    this->body = body;
-}
-
-std::string Response::toString(const Response& response) const
-{
-    std::ostringstream oss;
-    std::map<std::string, std::string>::const_iterator it = response.getHeaders().begin();
-    while (it != response.getHeaders().end()) {
-        oss << it->first << ": " << it->second << "\r\n";
-        ++it;
+std::string Response::getResponse(const Response& response) const {
+    std::stringstream ss;
+    ss << "HTTP/1.1 " << response.code << " " << response._statusMessage << "\r\n";
+    for (const auto& header : response.headers) {
+        ss << header.first << ": " << header.second << "\r\n";
     }
-    oss << "\r\n" << response.getBody();
-    return oss.str();
+    ss << "\r\n" << response.body;
+    return ss.str();
 }
 
+std::map<int, std::string> Response::_initStatusMap() {
+    std::map<int, std::string> statusMap;
+    statusMap[200] = "OK";
+    statusMap[404] = "Not Found";
+    statusMap[500] = "Internal Server Error";
+    return statusMap;
+}
+
+std::map<std::string, std::string> Response::_initMimeMap() {
+    std::map<std::string, std::string> mimeMap;
+    mimeMap["html"] = "text/html";
+    mimeMap["css"] = "text/css";
+    mimeMap["js"] = "application/javascript";
+    mimeMap["jpg"] = "image/jpeg";
+    mimeMap["png"] = "image/png";
+    return mimeMap;
+}
+
+std::map<int, std::string> Response::_statusMap = Response::_initStatusMap();
+std::map<std::string, std::string> Response::_mimeMap = Response::_initMimeMap();
+
+
+std::string Response::_buildHeader(size_t bodySize) const {
+    std::stringstream ss;
+    ss << "HTTP/1.1 " << code << " " << _statusMessage << "\r\n";
+    for (const auto& header : headers) {
+        ss << header.first << ": " << header.second << "\r\n";
+    }
+    ss << "Content-Length: " << bodySize << "\r\n";
+    ss << "\r\n";
+    return ss.str();
+}
+
+void Response::_buildError(int code) {
+    this->code = code;
+    _statusMessage = _statusMap[code];
+    body = "<html><body><h1>" + std::to_string(code) + " " + _statusMessage + "</h1></body></html>";
+    headers["Content-Type"] = "text/html";
+}
+
+std::string Response::_getMimeType(const std::string& path) const {
+    size_t dotPos = path.find_last_of('.');
+    if (dotPos != std::string::npos) {
+        std::string ext = path.substr(dotPos + 1);
+        if (_mimeMap.count(ext)) {
+            return _mimeMap[ext];
+        }
+    }
+    return "application/octet-stream";
+}
+
+std::string Response::_getDate() const {
+    return "Wed, 21 Oct 2015 07:28:00 GMT";
+}
+
+std::string Response::_readFile(const std::string& path) const {
+    return "File content of " + path;
+}
+
+
+void Response::_resetHeaders() {
+    headers.clear();
+}
--- Response.hpp
+++ Response.hpp
@@ -14,27 +14,41 @@ union Body
 class Response {
     private:
         int code;
-        std::string message;
+        std::string _statusMessage;
         std::map<std::string, std::string> headers;
+        std::string _response;
+        std::string _path;
+        std::string _contentType;
+        std::string _location;
+        std::string _allow;
         std::string body;
-        int fd;
-        int flag_fg;
 
+        static std::map<int, std::string>               _statusMap;
+        static std::map<std::string, std::string>       _mimeMap;
+
+
+        std::string _buildHeader(size_t bodySize) const;
+        void        _buildError(int code);
+        std::string _getMimeType(const std::string& path) const;
+        std::string _getDate() const;
+        std::string _readFile(const std::string& path) const;
+        bool        _fileExists(const std::string& path) const;
+        void        _resetHeaders();
+
+       
+        static std::map<int, std::string>             _initStatusMap();
+        static std::map<std::string, std::string>     _initMimeMap();
+
+        
     public:
         Response();
         ~Response();
         Response(const Response& other);
         Response& operator=(const Response& other);
-        Response(int code, const std::string& message);
-        void addHeader(const std::string& key, const std::string& value);
-        void setBody(const std::string& body);
-        void send() const;
+       
         int getCode() const;
 
-        const std::string& getMessage() const;
-        const std::map<std::string, std::string>& getHeaders() const;
-        const std::string& getBody() const;
-        std::string toString(const Response& response) const;
+        std::string getResponse(const Response& response) const;
 
 // 
 // get 
